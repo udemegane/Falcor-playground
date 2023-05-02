c3c82ac75cc3417db098527599cd4262a1724192
Yu Yanagida <cappuccino1204.ck@gmail.com>
2023-05-02 21:17:52 +0900

    init restir di pass

--- Data/ReSTIRGI_RT.py
+++ Data/ReSTIRGI_RT.py
@@ -2,7 +2,7 @@ from falcor import *
 
 def render_graph_ReSTIRGIaccum():
     g = RenderGraph('ReSTIRGIaccum')
-    ImageLoader = createPass('ImageLoader', {'outputSize': IOSize.Default, 'outputFormat': ResourceFormat.BGRA8UnormSrgb, 'filename': 'E:/Falcor/Source/RenderPasses/Falcor-playground/Data/FreeBlueNoiseTextures/Data/1024_1024/LDR_RGBA_0.png', 'mips': False, 'srgb': True, 'arrayIndex': 0, 'mipLevel': 0})
+    ImageLoader = createPass('ImageLoader', {'outputSize': IOSize.Default, 'outputFormat': ResourceFormat.BGRA8UnormSrgb, 'filename': 'C:/Users/cappu/Documents/workspace/Falcor_2/Source/RenderPasses/Falcor-playground/Data/FreeBlueNoiseTextures/Data/1024_1024/LDR_RGBA_0.png', 'mips': False, 'srgb': True, 'arrayIndex': 0, 'mipLevel': 0})
     g.addPass(ImageLoader, 'ImageLoader')
     ToneMapper = createPass('ToneMapper', {'outputSize': IOSize.Default, 'useSceneMetadata': True, 'exposureCompensation': 0.0, 'autoExposure': False, 'filmSpeed': 100.0, 'whiteBalance': False, 'whitePoint': 6500.0, 'operator': ToneMapOp.Aces, 'clamp': True, 'whiteMaxLuminance': 1.0, 'whiteScale': 11.199999809265137, 'fNumber': 1.0, 'shutter': 1.0, 'exposureMode': ExposureMode.AperturePriority})
     g.addPass(ToneMapper, 'ToneMapper')
